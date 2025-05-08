62e659d1183b912c53659166ec073149a1e01182
Robin Ochieng <robinochieng73@gmail.com>
2025-05-08 09:28:54 +0300

    Updated the custom value box with external css classes

--- modules/customValueBox.R
+++ modules/customValueBox.R
@@ -1,13 +1,23 @@
 # customValueBox.R
+# customValueBox <- function(title, value, box_color) {
+#   # Create a card-like HTML block using the provided title, value, and box color.
+#   HTML(paste0(
+#     "<div style='padding: 20px; border: 2px solid ", box_color, 
+#          "; border-radius: 5px; background-color: #f9f9f9;'>",
+#       "<h4 style='font-size: 20px; margin-top: 0; margin-bottom: 10px; color: ", box_color, ";'>", title, "</h4>",
+#       "<p style='font-size: 16px; font-weight: bold; margin-bottom: 0; color: ", box_color, ";'>", 
+#         value,
+#       "</p>",
+#     "</div>"
+#   ))
+# }
+
+
 customValueBox <- function(title, value, box_color) {
-  # Create a card-like HTML block using the provided title, value, and box color.
   HTML(paste0(
-    "<div style='padding: 20px; border: 2px solid ", box_color, 
-         "; border-radius: 5px; background-color: #f9f9f9;'>",
-      "<h4 style='font-size: 20px; margin-top: 0; margin-bottom: 10px; color: ", box_color, ";'>", title, "</h4>",
-      "<p style='font-size: 16px; font-weight: bold; margin-bottom: 0; color: ", box_color, ";'>", 
-        value,
-      "</p>",
+    "<div class='custom-value-box' style='border-color:", box_color, "'>",
+      "<h4 class='custom-value-title' style='color:", box_color, "'>", title, "</h4>",
+      "<p class='custom-value-value' style='color:", box_color, "'>", value, "</p>",
     "</div>"
   ))
 }
